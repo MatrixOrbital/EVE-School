25bfd3ecdfcd450789bdd26d167a6622b84cb29c
ddivino <35470642+ddivino@users.noreply.github.com>
2019-05-10 11:49:25 -0600

    Update Eve2_81x.h
    - Adding other TFT Display timings
    - Added 2.9", 3.8", 5.0", and 7.0" display timings.

--- Eve2_81x.h
+++ Eve2_81x.h
@@ -355,9 +355,64 @@ void CoProWrCmdBuf(const uint8_t *buffer, uint32_t count);
 uint32_t WriteBlockRAM(uint32_t Add, const uint8_t *buff, uint32_t count);
 
 // Panel specific setup parameters
-
+  
+//User selectable configurations.
+//#define EVE2_70
+#define EVE2_50
 //#define EVE2_43
-#ifdef EVE2_43 
+//#define EVE2_38
+//#define EVE2_35
+//#define EVE2_29
+
+#ifdef EVE2_70
+#define DWIDTH     800
+#define DHEIGHT    480
+#define PIXVOFFSET   0
+#define PIXHOFFSET   0
+#define HCYCLE     928
+#define HOFFSET     88
+#define HSYNC0       0
+#define HSYNC1      48
+#define VCYCLE     525
+#define VOFFSET     32
+#define VSYNC0       0
+#define VSYNC1       3
+#define PCLK         2
+#define SWIZZLE      0
+#define PCLK_POL     1
+#define HSIZE      800
+#define VSIZE      480
+#define CSPREAD      0
+#define DITHER       1
+#endif
+
+#ifdef EVE2_50
+#define DWIDTH     800
+#define DHEIGHT    480
+#define PIXVOFFSET   0
+#define PIXHOFFSET   0
+#define HCYCLE     928
+#define HOFFSET     88
+#define HSYNC0       0
+#define HSYNC1      48
+#define VCYCLE     525
+#define VOFFSET     32
+#define VSYNC0       0
+#define VSYNC1       3
+#define PCLK         2
+#define SWIZZLE      0
+#define PCLK_POL     1
+#define HSIZE      800
+#define VSIZE      480
+#define CSPREAD      0
+#define DITHER       1
+#endif
+
+#ifdef EVE2_43
+#define DWIDTH     480
+#define DHEIGHT    272
+#define PIXVOFFSET   0
+#define PIXHOFFSET   0
 #define HCYCLE     548
 #define HOFFSET     43
 #define HSYNC0       0
@@ -375,8 +430,33 @@ uint32_t WriteBlockRAM(uint32_t Add, const uint8_t *buff, uint32_t count);
 #define DITHER       1
 #endif
 
-#define EVE2_35
-#ifdef EVE2_35 
+#ifdef EVE2_38
+#define DWIDTH     480
+#define DHEIGHT    116
+#define PIXVOFFSET  10
+#define PIXHOFFSET   0
+#define HCYCLE     524
+#define HOFFSET     43
+#define HSYNC0       0
+#define HSYNC1      41
+#define VCYCLE     292
+#define VOFFSET     12
+#define VSYNC0     152
+#define VSYNC1      10
+#define PCLK         5
+#define SWIZZLE      0
+#define PCLK_POL     1
+#define HSIZE      480
+#define VSIZE      272
+#define CSPREAD      1
+#define DITHER       1
+#endif
+
+#ifdef EVE2_35
+#define DWIDTH     320
+#define DHEIGHT    240
+#define PIXVOFFSET   0
+#define PIXHOFFSET   0
 #define HCYCLE     408
 #define HOFFSET     68
 #define HSYNC0       0
@@ -393,6 +473,29 @@ uint32_t WriteBlockRAM(uint32_t Add, const uint8_t *buff, uint32_t count);
 #define CSPREAD      1
 #define DITHER       1
 #endif
+
+#ifdef EVE2_29
+#define DWIDTH     320
+#define DHEIGHT    102
+#define PIXVOFFSET   0
+#define PIXHOFFSET   0
+#define HCYCLE     408
+#define HOFFSET     70
+#define HSYNC0       0
+#define HSYNC1      10
+#define VCYCLE     262
+#define VOFFSET    156
+#define VSYNC0       0
+#define VSYNC1       2
+#define PCLK         8
+#define SWIZZLE      0
+#define PCLK_POL     0
+#define HSIZE      320
+#define VSIZE      102
+#define CSPREAD      1
+#define DITHER       1
+#endif
+
 
 #ifdef __cplusplus
 }
